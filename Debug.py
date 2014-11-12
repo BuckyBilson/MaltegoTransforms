decda18e9cb07b8919996c34c194725a20e84d90
BuckyBilson <buchanan.william@gmai.com>
2014-11-12 11:29:05 +0000

    Create Debug.py
    Point this at a Maltego entity and creates an entity for each piece of data and also labels with sys.argv position and (for sys.argv[2] the "hash position").

---
+++ Debug.py
@@ -0,0 +1,24 @@
+from twitter import *
+import os
+import sys
+from collections import Counter
+
+print"<MaltegoMessage>"
+print"<MaltegoTransformResponseMessage>"
+print"	<Entities>"
+x=0
+y=0
+for value in sys.argv:
+	print value
+	morevalues = value.split("#")
+	for morevalue in morevalues:
+		print"<Entity Type=\"maltego.Phrase\">" 
+		print"<Value>"+str(morevalue)+" Position:"+str(x)+" HashPosition:"+str(y)+"</Value>"
+		print"</Entity>"
+		y=y+1
+	x=x+1
+	y = 0
+print"	</Entities>"
+print"</MaltegoTransformResponseMessage>"
+print"</MaltegoMessage>"
+	
